5cb15a3f98fef353ac8ff22f4440018adfabed1d
justus.voss <justus.voss@check24.de>
2024-01-30 20:42:56 +0100

    Added Api base structure

---
+++ src/main/java/stockanalyzer/com/stockanalyzer/StockAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package stockanalyzer.com.stockanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAnalyzerApplication.class, args);
+    }
+
+}
